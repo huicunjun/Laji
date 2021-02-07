f481f0710cefe3a423821e4238c71f5e779d7f21
威威君 <1214585092@qq.com>
2021-02-07 18:45:07 +0800

    1

--- app/src/main/java/com/example/laji/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/laji/ui/gallery/GalleryFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,8 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.laji.R;
+import com.google.android.material.textfield.TextInputEditText;
+
 //垃圾分类识别
 public class GalleryFragment extends Fragment {
 
@@ -23,13 +26,30 @@ public class GalleryFragment extends Fragment {
         galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+
         return root;
     }
+    TextInputEditText editText;
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        editText = getView().findViewById(R.id.ed);
+
+        getView().findViewById(R.id.oklll).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = editText.getText().toString();
+                if (s.equals("")) {
+                    Toast.makeText(getActivity(), "请输入！", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            }
+        });
+    }
+
+
+
+
 }
